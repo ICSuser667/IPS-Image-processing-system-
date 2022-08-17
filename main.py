7df1d33264f4edf06ba9837dcb04d6556678de11
Ryan <ryanmccarthy667@gmail.com>
2022-08-17 21:49:50 +1200

    updated gui and file loading

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from PyQt6 import QtCore, QtGui, QtWidgets
 from PyQt6 import uic
 from PyQt6.QtWidgets import QFileDialog
 import yaml
-
+import subprocess
 
 class MainWindow(QtWidgets.QMainWindow):
 
@@ -14,32 +14,37 @@ class MainWindow(QtWidgets.QMainWindow):
 
     # method to edit dataset yaml
     def editYaml(self,folder_name,value):
-        with open("./yolov5/data/dataset.yaml") as f:
+        with open("yolov5/yoloconfig.yaml") as f:
             doc = yaml.safe_load(f)
             doc[value] = folder_name
-        with open("./yolov5/data/dataset.yaml", "w") as f:
+        with open("yolov5/yoloconfig.yaml", "w") as f:
             yaml.dump(doc, f, default_flow_style=False, sort_keys=False)
 
     # method that lets the user select the directory of the train data on train data button push
     def loadTrainingdata(self):
-        folder_name = QFileDialog.getExistingDirectory(self, "select folder with train data")
-        print("Loading Training data from: ", folder_name)
+        folder_name = QFileDialog.getExistingDirectory(self, "select folder with training data")
+        self.listWidget.addItem("Loading Training data from: ", folder_name)
         self.editYaml(folder_name, "train")
 
     def loadValidationData(self):
-        folder_name = QFileDialog.getExistingDirectory(self, "select folder with train data")
-        print("Loading validation data from: ", folder_name)
+        folder_name = QFileDialog.getExistingDirectory(self, "select folder with validation data")
+        self.listWidget.addItem("Loading validation data from: ", folder_name)
         self.editYaml(folder_name, "val")
 
     def sloadTestData(self):
-        folder_name = QFileDialog.getExistingDirectory(self, "select folder with train data")
-        print("Loading Test data from: ", folder_name)
+        folder_name = QFileDialog.getExistingDirectory(self, "select folder with testing data")
+        self.listWidget.addItem("Loading Test data from: ", folder_name)
         self.editYaml(folder_name, "test")
 
     def loadResults(self):
         folder_name = QFileDialog.getExistingDirectory(self, "select folder with train data")
-        print("Loading Training data from: ", folder_name)
+        self.listWidget.addItem("Loading Training data from: ", folder_name)
         self.editYaml(folder_name, "train")
+    def runModel(self):
+        self.listWidget.addItem("Starting Model")
+        command = "cd yolov5 && pip3 install -r requirements.txt &&python train.py --hyp /home/ryanscodingden/PycharmProjects/IPS-Image-processing-system-/yolov5/data/hyps/hyp.scratch-low.yaml --img 640 --batch 16 --epochs 3 --data yoloconfig.yaml --weights yolov516.pt"
+        subprocess.run(command, shell=True)
+
 
 app = QtWidgets.QApplication(sys.argv)
 window = MainWindow()
